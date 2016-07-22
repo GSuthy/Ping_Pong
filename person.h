7e06da22526c60ec7c40654263d01cd2c60c681a
Hyrum Joseph Crook <crookh@byu.local>
2016-07-22 11:15:30 -0600

    Added person class

---
+++ person.h
@@ -0,0 +1,17 @@
+#include "match.h"
+
+class Person
+{
+  private:
+  int wins;
+  int losses;
+  string name;
+
+  public:
+  int getWins();
+  void setWins();
+  int getLosses();
+  void setLosses();
+
+  Person(name);
+};
